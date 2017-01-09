4fa0893940d79618d18dd6c295965b1bb1dfa491
gj <1285803721@qq.com>
2017-01-09 20:18:43 +0800

    project for monitoring the state of search engine

---
+++ src/main/java/com/milanosoft/RCS/web/webSocket/hndler/SystemWebSocketHandler.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.milanosoft.RCS.web.webSocket.hndler;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.WebSocketMessage;
+import org.springframework.web.socket.WebSocketSession;
+
+import com.ldl.origami.util.Constants;
+
+/**
+ *
+ * @author dayu
+ */
+@Component
+public class SystemWebSocketHandler implements WebSocketHandler {
+	 
+    private static final ArrayList<WebSocketSession> users;
+ 
+    static {
+        users = new ArrayList<WebSocketSession>();
+    }
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        System.out.println("connect to the websocket success......");
+        users.add(session);
+        session.sendMessage(new TextMessage("Server:connected OK!"));
+    }
+
+    @Override
+    public void handleMessage(WebSocketSession wss, WebSocketMessage<?> wsm) throws Exception {
+        TextMessage returnMessage = new TextMessage(wsm.getPayload()
+				+ " received at server");
+		wss.sendMessage(returnMessage);
+    }
+
+    @Override
+    public void handleTransportError(WebSocketSession wss, Throwable thrwbl) throws Exception {
+        if(wss.isOpen()){
+            wss.close();
+        }
+       System.out.println("websocket connection closed......");
+       users.remove(wss);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession wss, CloseStatus cs) throws Exception {
+        System.out.println("websocket connection closed......");
+        users.remove(wss);
+    }
+
+    @Override
+    public boolean supportsPartialMessages() {
+        return false;
+    }
+    /**
+     * 给所有在线用户发送消息
+     *
+     * @param message
+     */
+    public void sendMessageToUsers(TextMessage message) {
+        for (WebSocketSession user : users) {
+            try {
+                if (user.isOpen()) {
+                    user.sendMessage(message);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+ 
+    /**
+     * 给某个用户发送消息
+     *
+     * @param userName
+     * @param message
+     */
+    public void sendMessageToUser(String userName, TextMessage message) {
+        for (WebSocketSession user : users) {
+            if (user.getAttributes().get(Constants.WEBSOCKET_USERNAME).equals(userName)) {
+                try {
+                    if (user.isOpen()) {
+                        user.sendMessage(message);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
+            }
+        }
+    }
+}
